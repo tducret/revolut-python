8985d313ee4e8a80212fd40d18b5b7a05ba84ead
thibdct <thibault.ducret@gmail.com>
2018-07-09 08:34:12 +0200

    Added CLI tool

---
+++ revolut.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import click
+from revolut import Revolut, __version__
+
+# Use
+# revolut.py --deviceid <device_id> --token <token>
+# revolut.py (with environment variables set :
+# REVOLUT_DEVICE_ID, REVOLUT_TOKEN)
+
+
+@click.command()
+@click.option(
+    '--deviceid', '-d',
+    envvar="REVOLUT_DEVICE_ID",
+    type=str,
+    help='your device id',
+    prompt='your device id'
+)
+@click.option(
+    '--token', '-t',
+    envvar="REVOLUT_TOKEN",
+    type=str,
+    help='your Revolut token',
+    prompt='your Revolut token'
+)
+@click.version_option(
+    version=__version__,
+    message='%(prog)s, based on [revolut] package version %(version)s'
+)
+def main(deviceid, token):
+    """ Get the account balances on Revolut """
+    rev = Revolut(device_id=deviceid, token=token)
+    account_balances = rev.get_account_balances()
+
+    print(account_balances.csv())
+
+
+if __name__ == "__main__":
+    main()
